cca7a70d79078e18c0cfb92b8421219946b07bc8
victormssa <victormssalves@outlook.com>
2022-06-07 17:08:22 -0300

    commit

--- server/sql/tabelas.js
+++ server/sql/tabelas.js
@@ -5,6 +5,7 @@ class Tabelas {
     this.criarAtendimentos();
   }
 
+
   criarAtendimentos() {
     const sql =
       "CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, hardware varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))";
