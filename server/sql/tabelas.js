05fb1a4bb9bb81259e77546e0e7fcea670606d5b
Ivan Ribeiro <82547732+ivanribeirotrc@users.noreply.github.com>
2022-06-08 16:25:52 -0300

    Ajuste nas tabelas
    adicionado NOT NULL em preco e descricao

--- server/sql/tabelas.js
+++ server/sql/tabelas.js
@@ -8,7 +8,7 @@ class Tabelas {
 
   criarProdutos() {
     const sql =
-      "CREATE TABLE IF NOT EXISTS Produtos (id int NOT NULL AUTO_INCREMENT, produto varchar(50) NOT NULL, preco varchar(20), condicao varchar(20) NOT NULL, marca varchar(20) NOT NULL, descricao text, data datetime NOT NULL, dataCriacao datetime NOT NULL, PRIMARY KEY(id))";
+      "CREATE TABLE IF NOT EXISTS Produtos (id int NOT NULL AUTO_INCREMENT, produto varchar(50) NOT NULL, preco varchar(20) NOT NULL, condicao varchar(20) NOT NULL, marca varchar(20) NOT NULL, descricao text NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, PRIMARY KEY(id))";
 
     this.conexao.query(sql, (erro) => {
       if (erro) {
