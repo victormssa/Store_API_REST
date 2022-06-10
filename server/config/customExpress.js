ecb734bd8fe40757465c7f5c890147cde31b1cb9
victormssa <victormssalves@outlook.com>
2022-06-10 13:01:17 -0300

    commit

--- server/config/customExpress.js
+++ server/config/customExpress.js
@@ -7,7 +7,29 @@ module.exports = () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  consign().include("controllers").into(app);
+  // Adiciona headers antes das rotas serem definidas.
+  app.use(function (req, res, next) {
+
+    // Website que você deseja conectar-se.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Métodos de Request que você deseja aceitar.
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Solicitação de headers que você deseja.
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true; Se você precisar que o site inclua cookies nas solicitações enviadas.
+    // para a API. (Por ex: em caso de você usar sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Passa para próxima camada de middleware.
+    next();
+  });
+
+  consign()
+    .include("controllers")
+    .into(app);
 
   return app;
 };
--- server/sql/tabelas.js
+++ server/sql/tabelas.js
@@ -1,23 +1,34 @@
 class Tabelas {
   init(conexao) {
     this.conexao = conexao;
+    this.inserir = conexao;
 
     this.criarProdutos();
-  }
-
+    this.inserirAuto();
+  } 
 
   criarProdutos() {
     const sql =
-      "CREATE TABLE IF NOT EXISTS Produtos (id int NOT NULL AUTO_INCREMENT, produto varchar(50) NOT NULL, preco varchar(20) NOT NULL, condicao varchar(20) NOT NULL, marca varchar(20) NOT NULL, descricao text NOT NULL, imagem text NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, PRIMARY KEY(id))";
+      "CREATE TABLE IF NOT EXISTS Produtos (id int NOT NULL AUTO_INCREMENT, produto varchar(30) NOT NULL, preco varchar(20) NOT NULL, condicao varchar(20) NOT NULL, marca varchar(20) NOT NULL, descricao text NOT NULL, imagem text NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, PRIMARY KEY(id))";
 
-    const inserir = 
-      "UPSERT INTO Produtos (produto, preco, condicao, marca, descricao, imagem, data) VALUES (,)";
-        
-    this.conexao.query(sql, inserir, (erro) => {
+    this.conexao.query(sql, (erro) => {
       if (erro) {
         console.log(erro);
       } else {
-        console.log("Tabela Produtos e Valores foram criados com sucesso");
+        console.log("Tabela Produtos foi criado com sucesso");
+      }
+    });
+  }
+
+  inserirAuto() {
+    const inserir = 
+      "INSERT INTO Produtos (produto, preco, condicao, marca, descricao, imagem, data) VALUES ('RTX 3090', '15.000 Reais', 'Lançamento', 'Descrição', 'https://i.ebayimg.com/images/g/D3QAAOSw9gJf6c55/s-l600.jpg', '25/04/2023')";
+
+    this.inserir.query(inserir, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log("Valores foram inseridos com sucesso");
       }
     });
   }
